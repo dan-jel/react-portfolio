7d44dd0db200ca14ff413012d6ff705e7dcde730
danjel <daniel.heese1@gmail.com>
2021-01-11 11:59:05 +0100

    einrichtung

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          lets go!
-        </p>
+        <p>lets go!</p>
       </header>
     </div>
   );

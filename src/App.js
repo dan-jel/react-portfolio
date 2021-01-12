dd8f4b986aa1bc188a7901da010707652c3dbb58
danjel <daniel.heese1@gmail.com>
2021-01-12 13:01:43 +0100

    dynamic project pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Nav from "./components/Nav";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
 import { Switch, Route } from "react-router-dom";
+import MovieDetail from "./pages/MovieDetail";
 
 function App() {
   return (
@@ -15,8 +16,11 @@ function App() {
         <Route path="/" exact>
           <AboutUs />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
         </Route>
         <Route path="/contact">
           <ContactUs />
---
+++ src/pages/MovieDetail.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+import { MovieState } from "../movieState";
+
+const MovieDetail = () => {
+  const history = useHistory();
+  const url = history.location.pathname;
+  const [movies, setMovies] = useState(MovieState);
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    const currentMovie = movies.filter((stateMovie) => stateMovie.url === url);
+    setMovie(currentMovie[0]);
+  }, [movies, url]);
+
+  return (
+    <>
+      {movie && (
+        <Details>
+          <Headline>
+            <h2>{movie.title}</h2>
+            <img src={movie.mainImg} alt="movie" />
+          </Headline>
+          <Awards>
+            {movie.awards.map((award) => (
+              <Award
+                title={award.title}
+                description={award.description}
+                key={award.title}
+              />
+            ))}
+          </Awards>
+          <ImageDisplay>
+            <img src={movie.secondaryImg} alt="second" />
+          </ImageDisplay>
+        </Details>
+      )}
+    </>
+  );
+};
+
+const ImageDisplay = styled.div`
+  min-height: 50vh;
+  img {
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+  }
+`;
+
+const Details = styled.div`
+  color: white;
+`;
+
+const Headline = styled.div`
+  min-height: 90vh;
+  padding-top: 3rem;
+  position: relative;
+  h2 {
+    width: 100%;
+    text-align: center;
+    padding-bottom: 3rem;
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
+
+const Awards = styled.div`
+  min-height: 80vh;
+  display: flex;
+  margin: 5rem 10rem;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const AwardStyle = styled.div`
+  padding: 5rem;
+  h3 {
+    font-size: 2rem;
+  }
+  .line {
+    width: 70%;
+    background: #23d997;
+    height: 0.5rem;
+    margin: 1rem 0rem;
+  }
+  p {
+    padding: 2rem 0rem;
+  }
+`;
+
+const Award = ({ title, description }) => {
+  return (
+    <AwardStyle>
+      <h3>{title}</h3>
+      <div className="line"></div>
+      <p>{description}</p>
+    </AwardStyle>
+  );
+};
+
+export default MovieDetail;
